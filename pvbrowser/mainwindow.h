3d1cc4fd4999eb1bebc039582c745eda54096e26
pvbrowser rl <lehrig@t-online.de>
2011-10-10 16:34:23 +0200

    pvbrowser 4.6.4

--- pvbrowser/mainwindow.h
+++ pvbrowser/mainwindow.h
@@ -16,7 +16,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#define VERSION "4.6.3"
+#define VERSION "4.6.4"
 
 #include "interpreter.h"
 
--- pvserver/util.cpp
+++ pvserver/util.cpp
@@ -19,7 +19,7 @@
 
 #include <locale.h>
 
-const char pvserver_version[] = "4.6.3";
+const char pvserver_version[] = "4.6.4";
 
 /* #include "qimage.h" */
 #include <time.h>
