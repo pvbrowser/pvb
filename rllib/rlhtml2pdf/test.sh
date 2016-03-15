c224e93d39a70739b29e8f19772ecb8221733aff
Rainer Lehrig <lehrig@t-online.de>
2016-03-15 09:39:55 +0100

    Vogelweide

---
+++ rllib/rlhtml2pdf/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -f herzelieb*
+rm -f Herzelieb*
+wget https://de.wikipedia.org/wiki/Herzeliebez_vrouwelin
+rlhtml2pdf Herzeliebez_vrouwelin herzeliebs_frowelin.pdf
+echo Under der linden
+echo an der heide,
+echo dâ unser zweier bette was,
+echo dâ mugent ir vinden
+echo schône beide
+echo gebrochen bluomen unde gras.
+echo vor dem walde in einem tal,
+echo tandaradei,
+echo schône sanc diu nahtegal.
